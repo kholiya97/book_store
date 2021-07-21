4ff1a2744e04ee99ad1af1f60f26073bd3f8057f
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-07-21 19:15:47 +0530

    [himanshu]:UserController is added

---
+++ BookStore/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using BookStoreBLinterface;
+using BookStoreCommonLayer.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        IUserBL userBl;
+        public UserController(IUserBL userBl)
+        {
+            this.userBl = userBl;
+        }
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public ActionResult AddUser(Users user)
+        {
+            try
+            {
+                this.userBl.AddUser(user);
+                return this.Ok(new { success = true, message = "Registration Successful " });
+            }
+
+            catch (Exception e)
+            {
+                return this.BadRequest(new { success = false, message = e.Message });
+            }
+        }
+    }
+}
